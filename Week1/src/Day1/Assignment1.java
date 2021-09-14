64e00796322a8734836be5c38492b867f824a6f8
KaxianC2 <90656225+KaxianC2@users.noreply.github.com>
2021-09-14 13:54:55 -0700

    Add files via upload

---
+++ Week1/src/Day1/Assignment1.java
@@ -0,0 +1,70 @@
+package Day1;
+/**
+* Assignment1 --- Program that Prints out '*' using looping construct
+*@author Kaxian Situ
+*/
+public class Assignment1{
+    public static void main(String[] args) throws Exception {
+        System.out.println("1)");
+		for(int i = 1; i <=4; i++)
+		{
+			for(int j= 0;j<i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+        printDot(9);
+		System.out.println("2)");
+        printDot(10);
+		for(int i = 4; i >0; i--)
+		{
+			for(int j= i;j>0;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("3)");
+		int space = 5;
+		for(int i = 0; i <4; i++)
+		{
+			for(int j=0;j<space-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0;k<i*2+1;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+        printDot(11);
+		System.out.println("4)");
+        printDot(12);
+		for(int i = 3; i >=0; i--)
+		{
+			for(int j=0;j<space-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0;k<i*2+1;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+    }
+	/**
+	 * Prints out '*' based on input
+	 * @param num An integer denoting how many '*'' to print
+	 */
+    public static void printDot(int num)
+    {
+        for(int i=0;i<=num;i++)
+        {
+            System.out.print(".");
+        }
+        System.out.println("");
+    }
+}
---
+++ Week1/src/Day1/Assignment2.java
@@ -0,0 +1,34 @@
+package Day1;
+/**
+*Assignment2 --- A number guessing game
+*@author Kaxian Situ
+*/
+import java.lang.Math; 
+import java.util.Scanner;
+public class Assignment2 {
+    public static void main(String[] args){
+        int ran = (int)(Math.random()*99+1);
+		Scanner input = new Scanner(System.in);
+		System.out.println("Guess a number between 1-100");
+		int count =0;
+		do{
+			int x = input.nextInt();
+			if(x>=ran-10 && x<=ran+10)
+			{
+				System.out.println("The correct number is: "+ ran);
+				break;
+			}
+			else if(count<4)
+			{
+				System.out.println("Incorrect number. Please Try again.");
+				count++;
+			}
+			else
+			{
+				System.out.println("Sorry, You've reached 5 tries. The Answer is: "+ ran);
+				break;
+			}
+		}while(true);
+		input.close();
+    }
+}
